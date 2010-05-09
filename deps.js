b8f656865e18ab08f6d687e07de484392524bd43
Slink <steinlink@slinkbook.westell.com>
2010-05-09 14:07:15 -0400

    added downstreampipeline

--- deps.js
+++ deps.js
@@ -10,14 +10,6 @@ function clone(name, url) {
     return function(callback, errorHandler) {
         var command = spawn("git", ["clone", url, "vendor/" + name]);
         
-        command.stdout.addListener('data', function (data) {
-            sys.print(" * " + data);
-        });
-        
-        command.stderr.addListener('data', function (data) {
-            sys.print(" ! " + data);
-        });
-        
         command.addListener('exit', function (code) {
             sys.puts("Finished");
             callback();
