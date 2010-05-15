55d3bf8f2b94f18b48a6d89b5324cb32a4b2d6e3
Slink <steinlink@toko.westell.com>
2010-05-15 13:56:35 -0400

    Updated dependency script

--- deps.js
+++ deps.js
@@ -2,31 +2,59 @@ var sys   = require('sys');
 var spawn = require('child_process').spawn;
 var fs    = require('fs');
 
+/**
+ * Dependency list
+ */
+deps = { "node-xml" : "http://github.com/robrighter/node-xml.git",
+         "do"       : "http://github.com/creationix/do.git" }
 
 
 
+/**
+ * Clone or update a dependency
+ */
+function getdep(name, url) {
 
-function clone(name, url) {
-    return function(callback, errorHandler) {
-        var command = spawn("git", ["clone", url, "vendor/" + name]);
-        
-        command.addListener('exit', function (code) {
-            sys.puts("Finished");
-            callback();
-        });
-    }
+    sys.puts("Downloading " + name + " from " + url);
+    var path = "vendor/" + name;
+
+    // make vendor dir, ignore errors
+    fs.mkdir("vendor", 0755, function(e) {});
+    
+    // make lib dir
+    fs.mkdir(path, 0755,  function(e) {
+        if (e === undefined) {
+            
+            // checkout the lib
+            var command = spawn("git", ["clone", url, path]);
+            
+            command.stdout.addListener("data", function (data) {
+                sys.puts(data);
+            });
+
+            command.addListener("exit", function (code) {
+                sys.puts("Cloned " + name);
+            });
+
+        } else {
+
+            // update the lib
+            var command = spawn("git", ["pull", path]);
+            
+            command.stdout.addListener("data", function (data) {
+                sys.puts(data);
+            });
+
+            command.addListener("exit", function (code) {
+                sys.puts("Updated " + name);
+            });
+        }
+    });
 }
 
-
-
-
-
 (function() {
-    fs.mkdirSync("vendor", 0755); 
-    
-    clone("node-xml", "http://github.com/robrighter/node-xml.git")(
-        function() {
-            clone("do", "http://github.com/creationix/do.git")(function() {}, function() {});
-        }, function() {});
+    for (var key in deps) {
+        getdep(key, deps[key]);
+    }
 })();
 
